9eab46dccc7ba30cb5260a2ca7ae505fb44e426e
Kaniska Mohanty <mohantyk@users.noreply.github.com>
2018-10-14 15:12:35 -0700

    Add waterfall led code.

---
+++ 03_LightWater.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 14 15:03:21 2018
+
+@author: kaniska
+"""
+
+import RPi.GPIO as GPIO
+import time
+
+pins = [11,13,15, 12, 16]
+
+def setup():
+    GPIO.setmode(GPIO.BOARD)
+    for pin in pins:
+        GPIO.setup(pin, GPIO.OUT)
+        GPIO.output(pin, GPIO.HIGH)
+        
+def loop():
+    while True:
+        for pin in pins:
+            GPIO.setup(pin, GPIO.LOW)
+            time.sleep(0.1)
+            GPIO.setup(pin, GPIO.HIGH)
+            
+        for pin in pins[::-1]:
+            GPIO.setup(pin, GPIO.LOW)
+            time.sleep(0.1)
+            GPIO.setup(pin, GPIO.HIGH)
+            
+def destroy():
+    for pin in pins:
+        GPIO.output(pin, GPIO.HIGH)
+        GPIO.cleanup()
+        
+
+
+if __name__=='__main__':
+    setup()
+    try:
+        loop()
+    except KeyboardInterrupt:
+        destroy()
+        
