fd742b122dfc552157ddd6da95565d8561191af5
Kaniska Mohanty <mohantyk@users.noreply.github.com>
2018-10-14 15:15:48 -0700

    Add debug prints

--- 03_LightWater.py
+++ 03_LightWater.py
@@ -9,7 +9,7 @@ Created on Sun Oct 14 15:03:21 2018
 import RPi.GPIO as GPIO
 import time
 
-pins = [11,13,15, 12, 16]
+pins = [11,13,15,12,16]
 
 def setup():
     GPIO.setmode(GPIO.BOARD)
@@ -20,19 +20,24 @@ def setup():
 def loop():
     while True:
         for pin in pins:
+            print('Turning pin {} on.'.format(pin))
             GPIO.setup(pin, GPIO.LOW)
             time.sleep(0.1)
+            print('Turning pin {} off.'.format(pin))
             GPIO.setup(pin, GPIO.HIGH)
             
         for pin in pins[::-1]:
+            print('Turning pin {} on.'.format(pin))
             GPIO.setup(pin, GPIO.LOW)
             time.sleep(0.1)
+            print('Turning pin {} off.'.format(pin))
             GPIO.setup(pin, GPIO.HIGH)
             
 def destroy():
     for pin in pins:
         GPIO.output(pin, GPIO.HIGH)
         GPIO.cleanup()
+        print('\nCleaned up resources. Board ready for reuse.')
         
 
 
