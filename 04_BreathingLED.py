cb59dddb62092caa22c22107c7f51460f12142fd
mohantyk <km@raspberry.pi>
2018-10-14 21:43:01 -0700

    Experiment with few parameters. Add print statements.

--- 04_BreathingLED.py
+++ 04_BreathingLED.py
@@ -11,26 +11,29 @@ import time
 
 ledPin = 12
 sleepTime = 0.01
+freq = 1000
 
 def setup():
     global p
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(ledPin, GPIO.OUT)
     GPIO.output(ledPin, GPIO.LOW)
-    p = GPIO.PWM(ledPin, 1000)
+    p = GPIO.PWM(ledPin, freq)
     p.start(0)
     
 def loop():
     while True:
+        print('Increasing duty cycle')
         for duty_cycle in range(0,101,1):
             p.ChangeDutyCycle(duty_cycle)
             time.sleep(sleepTime)
-        time.sleep(1)
+        time.sleep(0.5)
         
+        print('Decreasing duty cycle')
         for duty_cycle in range(100,-1,-1):
             p.ChangeDutyCycle(duty_cycle)
             time.sleep(sleepTime)
-        time.sleep(1)
+        time.sleep(0.5)
     
     
 def destroy():
